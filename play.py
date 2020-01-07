30fedf304af140ab691a54e616102f7e1e806bd2
bosua3393 <bosua3393@gmail.com>
2020-01-06 21:22:31 -0500

    improve input

--- play.py
+++ play.py
@@ -15,12 +15,12 @@ def get_state(board_l, player):
 
 first = 1  # 1:ai first, 2:human first
 mode = 2  # 1:human, 2:AI
-name = 'best'
+name = 'Ai_model'
 if mode == 2:
     model = load_model(f'keras_model/{name}')
 board = TTT5()
 end = False
-print_output = False
+print_output = True
 while not end:
     player_turn = board.player
     if player_turn == first:
@@ -37,13 +37,15 @@ while not end:
             state = get_state(board.board[:], int(player_turn))
             output = model.predict(np.array(state))
             if print_output:
-                print(np.round(output[0][0:4], 2))
-                print(np.round(output[0][4:8], 2))
-                print(np.round(output[0][8:12], 2))
-                print(np.round(output[0][12:16], 2))
+                print(np.round(output[0][0:5], 2))
+                print(np.round(output[0][5:10], 2))
+                print(np.round(output[0][10:15], 2))
+                print(np.round(output[0][15:20], 2))
+                print(np.round(output[0][20:25], 2))
             action = np.argmax(output)
             ret = board.play(action)
             if 'invalid' in ret:
+                board.print_board()
                 print('Invalid move. AI lose')
                 exit()
             if 'win' in ret:
@@ -52,7 +54,7 @@ while not end:
                 print('draw')
     else:
         board.print_board()
-        ret = board.play(int(input('Human turn:')))
+        ret = board.play(int(input('Human turn:'))-1)
         if 'invalid' in ret:
             print('Invalid move. Try again')
         if 'win' in ret:
@@ -63,9 +65,9 @@ while not end:
         board.print_board()
         end = True
 '''
-0   1  2  3  4  
-5   6  7  8  9  
-10 11 12 13 14 
-15 16 17 18 19 
-20 21 22 23 24
+1  2  3  4  5   
+6  7  8  9  10 
+11 12 13 14 15 
+16 17 18 19 20 
+21 22 23 24 25
 '''
