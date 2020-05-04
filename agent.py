80b09bbd88cd647919379720b4b324c961849422
yui <bosua3393@gmail.com>
2020-05-03 23:29:03 -0400

    working

--- agent.py
+++ agent.py
@@ -8,42 +8,15 @@ import random
 from collections import deque
 
 
-rotate_location = [4, 9, 14, 19, 24, 3, 8, 13, 18, 23, 2, 7, 12, 17, 22, 1, 6, 11, 16, 21, 0, 5, 10, 15, 20]
-
-
-def rotate(board):
-    rotated_board = []
-    for location in rotate_location:
-        rotated_board.append(int(board[location]))
-    if len(board) > 25:
-        rotated_board.append(int(board[25]))
-    return rotated_board
-
-
-def get_rotate_boards(s, t):
-    states, targets = [], []
-    for i in range(len(s)):
-        os = s.popleft()
-        ot = t.popleft()
-        states.append(os[:])
-        targets.append(ot[:])
-        for rotation in range(3):
-            os = rotate(os)
-            ot = rotate(ot)
-            states.append(os[:])
-            targets.append(ot[:])
-    return states, targets
-
-
 class Agent:
     def __init__(self, state_size, action_size, model_name=None):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = []
-        self.memory2 = []
+        self.memory = deque(maxlen=5_000_000)
+        self.tree = {}
         self.model_name = model_name
         self.gamma = 0.95
-        self.data = 1_500_000
+        self.data = 2_500_000
         self.epsilon = 1.0
         self.epsilon_min = .25
         self.epsilon_decay = float(np.e)**float(np.log(self.epsilon_min/self.epsilon)/self.data)
@@ -63,8 +36,6 @@ class Agent:
         model = Sequential()
         model.add(Dense(units=512, input_dim=self.state_size, activation="relu"))
         model.add(Dense(units=512, activation="relu"))
-        model.add(Dense(units=512, activation="relu"))
-        model.add(Dense(units=512, activation="relu"))
         model.add(Dense(units=256, activation="relu"))
         model.add(Dense(units=256, activation="relu"))
         model.add(Dense(units=128, activation="relu"))
@@ -72,15 +43,32 @@ class Agent:
         model.compile(loss="mse", optimizer=Adam(lr=0.001))
         return model
 
-    def act(self, state):
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-            if random.random() <= self.epsilon:
-                empty_index = []
-                for i in range(25):
-                    if not state[0][i]:
-                        empty_index.append(i)
-                return empty_index[random.randrange(len(empty_index))]
+    def act(self, state, mem_id, p):
+        self.epsilon *= self.epsilon_decay
+
+        if self.epsilon > self.epsilon_min and random.random() <= self.epsilon:
+            if mem_id in self.tree:
+                if p:
+                    print('u')
+                mem = self.tree[mem_id]
+                return np.argmin(mem[1])
+            if p:
+                print('random')
+            empty_index = []
+            for i in range(25):
+                if not state[0][i]:
+                    empty_index.append(i)
+            return empty_index[random.randrange(len(empty_index))]
+
+        if mem_id in self.tree:
+            if p:
+                print('q')
+            mem = self.tree[mem_id]
+            q = np.nan_to_num(mem[2] / mem[1])
+            return np.argmax(q)
+
+        if p:
+            print('predict')
         output = self.model.predict(state)
         # print(np.round(output[0][0:5], 2))
         # print(np.round(output[0][5:10], 2))
@@ -90,34 +78,36 @@ class Agent:
         return np.argmax(output)
 
     def exp_replay(self):
+        self.make_tree()
         states = []
-        target_fs = []
-        next_states = []
-        # current_states = []
-        for event in self.memory:
-            # current_states.append(event[0][0][0])
-            for [_, _, _, next_state, done] in event:
-                if not done:
-                    next_states.append(next_state[0])
-        next_outputs = deque(self.model.predict(np.array(next_states), verbose=1))  # .tolist()
-        # state_outputs = deque(self.model.predict(np.array(current_states), verbose=1))  # .tolist()
-        for event in self.memory:
-            state = event[0][0]
-            # target_f = state_outputs.popleft()
-            target_f = [0] * 25
-            for [_, action, reward, _, done] in event:
-                if done:
-                    target = reward
-                else:
-                    # target = reward + (self.gamma * max(next_outputs.popleft()))
-                    target = max(min(reward + (self.gamma * max(next_outputs.popleft())), 1), -1)
-                target_f[action] = target
-            states.append(np.array(state[0][:]))
-            target_fs.append(np.array(target_f[:]))
-        for [s, t] in self.memory2:
-            states.append(s[0][:])
-            target_fs.append(t[:])
 
-        states, target_fs = get_rotate_boards(deque(states), deque(target_fs))
+        for mem_id in self.tree:
+            states.append(self.tree[mem_id][0][0])
 
-        self.model.fit([states], [target_fs], epochs=1, verbose=1, batch_size=8192)
+        targets = self.model.predict(np.array(states), verbose=1)
+        for i, mem_id in enumerate(self.tree):
+            mem = self.tree[mem_id]
+            for c, counter in enumerate(mem[1]):
+                if counter:
+                    targets[i][c] = mem[2][c] / mem[1][c]
+
+        # for s, t in zip(states, targets):
+        #     print(s, t)
+        self.model.fit([states], [targets], epochs=1, verbose=1, batch_size=8192)
+
+    def make_tree(self):
+        self.tree = {}
+        for [mem_id, state, action, reward] in self.memory:
+            if mem_id in self.tree:
+                self.tree[mem_id][1][action] += 1
+                self.tree[mem_id][2][action] += reward
+            else:
+                counters = np.zeros(self.action_size)
+                rewards = np.zeros(self.action_size)
+                counters[action] = 1
+                rewards[action] = reward
+                for i, location in enumerate(state[0][:-1]):
+                    if location:
+                        counters[i] = 10000000
+                        rewards[i] = -10000000
+                self.tree[mem_id] = [state, counters, rewards]
--- train.py
+++ train.py
@@ -5,36 +5,9 @@ import random
 
 
 def get_state(board, player):
-    if player == 1:
-        state = board[:]
-        state.append(-1)
-    else:
-        state = board[:]
-        state.append(1)
+    state = board
+    state.append(player)
     return np.array([state])
-
-
-def get_next_state(board, player, ai):
-    board_l = TTT5()
-    for i in range(len(board)):
-        board_l.board = board[:]
-        board_l.player = player
-        ret = board_l.play(i)
-        if 'win' in ret:
-            return None, ret
-    board_l.board = board[:]
-    board_l.player = player
-    state = get_state(board, player)
-    ret = board_l.play(np.argmax(ai.predict(state)))
-    if 'invalid' in ret:
-        empty_index = []
-        for i in range(len(board)):
-            if not board[i]:
-                empty_index.append(i)
-        ret = board_l.play(empty_index[random.randrange(len(empty_index))])
-    next_board = board_l.board
-    next_player = board_l.player
-    return get_state(next_board, next_player), ret
 
 
 def get_reward(ret, next_ret):
@@ -46,78 +19,61 @@ def get_reward(ret, next_ret):
         return 0
 
 
-def get_events(state, board, player, ai):
-    events_l = []
-    win_action = []
-    for i in range(len(board)):
-        new_board = TTT5()
-        new_board.board = board[:]
-        new_board.player = player
-        ret = new_board.play(i)
-        if 'invalid' in ret or 'draw' in ret:
-            reward = get_reward(ret, '')
-            events_l.append([state, i, reward, None, True][:])
-            continue
-        elif 'win' in ret:
-            win_action.append(i)
-        else:
-            next_state, next_ret = get_next_state(new_board.board[:], int(new_board.player), ai)
-            reward = get_reward(ret, next_ret)
-            if 'win' in next_ret or 'draw' in next_ret:
-                events_l.append([state, i, reward, None, True][:])
-            else:
-                events_l.append([state, i, reward, next_state, False][:])
-    if win_action:
-        target = []
-        for w in range(len(board)):
-            if w in win_action:
-                target.append(1)
-            else:
-                target.append(-1)
-        return [state, target], 2
-    return events_l, 1
+def get_id(state):
+    mem_id = ''
+    for i in state[0]:
+        mem_id += str(i + 1)
+    return mem_id
 
 
 def run():
     student = Agent(26, 25, model_name=name)
     game_n = 0
     while True:
-        games = 25 if student.epsilon <= student.epsilon_min else 4000
-        for _ in range(games):
+        games = 25 if student.epsilon <= student.epsilon_min else 10000
+        for g in range(games):
             board = TTT5()
             end = False
             game_n += 1
             turn = 0
-            print(game_n, len(student.memory), len(student.memory2), end='\r')
+            reward = 0
+            tem_mem = []
+            last_player = 0
+            print(game_n, 'mem:', len(student.memory), 'tree:', len(student.tree), student.epsilon, end='\r')
             while not end:
                 player_turn = int(board.player)
                 current_board = board.board[:]
                 state = get_state(current_board, player_turn)
-                if turn == 0:
-                    action = game_n % 25
-                else:
-                    action = student.act(np.array(state))
+                mem_id = get_id(state)
+                action = student.act(np.array(state), mem_id, g == 0)
                 ret = board.play(action)
-                events, mem_type = get_events(state, current_board, player_turn, student.model)
-                if mem_type == 1:
-                    student.memory.append(events)
-                    reward = events[action][2]
-                else:
-                    student.memory2.append(events)
-                    rewards = events[1]
-                    reward = rewards[action]
-                # print(f'{game_n}: {action} {ret} reward: {reward}')
-                # board.print_board()
+
+                tem_mem.append([mem_id, state, action][:])
+                if g == 0:
+                    print(f'{game_n}: {action} {ret} reward: {reward}')
+                    board.print_board()
+
                 if 'invalid' in ret:
                     break
-                if 'win' in ret or 'draw' in ret:
+
+                elif 'win' in ret:
+                    reward = 1
+                    last_player = player_turn
+                    end = True
+
+                elif 'draw' in ret:
+                    reward = 0
+                    last_player = player_turn
                     end = True
                 turn += 1
-        student.exp_replay()
-        test = get_state([0]*25, 1)
-        start_move = np.argmax(student.model.predict(test))
-        student.model.save(f'keras_model/{name}_{str(int(game_n))}_{int(start_move)}')
 
+            for [mem_id, state, action] in tem_mem:
+                player = state[0][-1]
+                reward = reward if player == last_player else - reward
+                student.memory.append([mem_id, state, action, reward])
+
+        student.exp_replay()
+        student.model.save(f'keras_model/{name}_{str(int(game_n))}')
 
 
 name = 'h'
